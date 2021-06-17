8ccb0cd1c52f0e4abb2ff47bbb043ac0c1c488f5
Matthew Pickering <matthewtpickering@gmail.com>
2021-06-17 21:55:28 +0100

    extend examples

--- simple/app/Main.hs
+++ simple/app/Main.hs
@@ -19,19 +19,28 @@ main = withGhcDebug $ do
   -- is the evaluated length of the list.
   let !example2 = length [0..len]
 
+  -- A simple list structure
+  let !example3  = [0, 1, 2, 3, 4, 5]
+
+  -- A simple pair structure
+  let !example4  = ('a', 'b')
+
+  -- A simple function
+  let !example5  = \x -> print len
+
   -- The internal structure of data structures can be inspected using
   -- ghc-debug, for example, we can look at what the inside of a Map looks
   -- like.
-  let !example3  = M.fromList [(n, ()) | n <- [0..len]]
+  let !example6  = M.fromList [(n, ()) | n <- [0..len]]
 
   -- StrictBox has a strict field, so forcing the value also causes the
   -- boxed value to be forced. This can be verified in ghc-debug
-  let !example4 = StrictBox (length [0..len])
+  let !example7 = StrictBox (length [0..len])
 
   -- Subtle point about strict fields! Doesn't mean we always avoid
   -- thunks. You have to make sure the constructor is forced in order
   -- to force the strict boxes. Just has a lazy field so
-  let !example5 = Just (StrictBox (length [0..len]))
+  let !example8 = Just (StrictBox (length [0..len]))
 
   -- Ex: How could you modify example5 to force the inner thunk?
 
@@ -41,6 +50,9 @@ main = withGhcDebug $ do
                , Box example3
                , Box example4
                , Box example5
+               , Box example6
+               , Box example7
+               , Box example8
                ]
 
   -- Stop executing, so we can pause
